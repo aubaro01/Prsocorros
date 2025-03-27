ac36835b8ba7a9ed19acb37e49ff44a3a2afcca1
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-03-27 11:47:03 +0000

    up

--- src/pages/PageEx4.js
+++ src/pages/PageEx4.js
@@ -35,7 +35,7 @@ export default function PageSBV() {
   const steps = [
     {
       id: 0,
-      title: "Vídeo Demonstrativo",
+      title: "Vídeo introdutório",
       content: (
         <>
           <div className="ratio ratio-16x9 mb-4">
