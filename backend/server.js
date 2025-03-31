471a35ae2ae20f8f654698ca26cf61486f5550b9
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-03-31 10:40:36 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,7 @@
-const app = require('./app'); 
+const app = require('./app');
 
-module.exports = app;
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
